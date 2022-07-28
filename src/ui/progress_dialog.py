deb84e14b26cef9452155c8703941e1d8898c17f
Stephen Kent <stevekcode@gmail.com>
2022-07-27 21:36:25 -0400

    Add standard buttons to progress dialog

--- src/ui/progress_dialog.py
+++ src/ui/progress_dialog.py
@@ -12,28 +12,41 @@ import math
 
 from aqt.qt import *
 
+from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class ProgressDialog(object):
     def setupUi(self, Dialog):
-        if not Dialog.objectName():
-            Dialog.setObjectName(u"Dialog")
-        Dialog.resize(393, 255)
-        self.progressBar = QProgressBar(Dialog)
-        self.progressBar.setObjectName(u"progressBar")
-        self.progressBar.setGeometry(QRect(30, 220, 331, 23))
-        self.progressBar.setValue(0)
-        self.textBrowser = QTextBrowser(Dialog)
-        self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setGeometry(QRect(30, 20, 331, 192))
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(365, 330)
+        Dialog.setAutoFillBackground(True)
+        Dialog.setSizeGripEnabled(True)
+        Dialog.setModal(True)
+        self.widget = QtWidgets.QWidget(Dialog)
+        self.widget.setGeometry(QtCore.QRect(10, 10, 341, 311))
+        self.widget.setObjectName("widget")
+        self.gridLayout = QtWidgets.QGridLayout(self.widget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.textBrowser = QtWidgets.QTextBrowser(self.widget)
+        self.textBrowser.setObjectName("textBrowser")
+        self.gridLayout.addWidget(self.textBrowser, 0, 0, 1, 1)
+        self.progressBar = QtWidgets.QProgressBar(self.widget)
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setObjectName("progressBar")
+        self.gridLayout.addWidget(self.progressBar, 1, 0, 1, 1)
+        self.buttonBox = QtWidgets.QDialogButtonBox(self.widget)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setCenterButtons(True)
+        self.buttonBox.setObjectName("buttonBox")
+        self.gridLayout.addWidget(self.buttonBox, 2, 0, 1, 1)
 
         self.retranslateUi(Dialog)
-
-        QMetaObject.connectSlotsByName(Dialog)
-    # setupUi
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
-    # retranslateUi
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Generating Cards..."))
 
     def updateProgress(self, word, current, total):
         self.textBrowser.append(f'finished: {word}')
