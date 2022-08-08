02f07bebcdd3daee3885b36654f49e369f5280ce
Stephen Kent <stevekcode@gmail.com>
2022-08-08 17:31:46 -0400

    Docstrings for hashing.py

--- src/hashing.py
+++ src/hashing.py
@@ -1,6 +1,14 @@
 import hashlib
 
-# Simple utility function to generate a hash string for media files
-def get_media_hash(text, bytes=8):
+def get_media_hash(text: str, bytes: int = 8) -> int:
+    """Generate a hash for the given media. Used to name the resulting file that will be attached to each card.
+
+    :param text: text the media was generated from
+    :type text: str
+    :param bytes: size of the generated hash, defaults to 8
+    :type bytes: int, optional
+    :return: MD5 hash for the file
+    :rtype: int
+    """
     encoded = text.encode()
     return int.from_bytes(hashlib.md5(encoded).digest()[ :bytes ], byteorder='big')
